ea915737aec910aec267c8308f2af3119e70a8e0
Luo Cheng <cheng.luo@intel.com>
2024-09-24 23:38:11 -0700

    fix dependency

--- ovllm/export/llama.py
+++ ovllm/export/llama.py
@@ -140,7 +140,7 @@ def get_params_from_model(path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('')
     parser.add_argument('--org_model_path', type=str, default='Model ID (can be a Hugginface Hub id, or a local directory)')
-    parser.add_argument('--ov_model_path', type=str, nargs='?', default='./gen/llama-2-7b-chat/')
+    parser.add_argument('--ov_model_path', type=str, nargs='?', default='./gen/llama-2-7b/')
     parser.add_argument('--quant_type', type=str, nargs='?', default='', choices=['','f16','nncf_w8', 'INT8_ASYM', 'INT8_SYM'])
     parser.add_argument("--fuse-qkv", action="store_true", help="fuse Q/K/V Linear projections into single Linear")
     args = parser.parse_args()
