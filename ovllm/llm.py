17b7159e93ccfc5cff6b6d03b5ef95bc0a24ede7
Luo Cheng <cheng.luo@intel.com>
2024-10-10 09:04:57 +0800

    fix computation for tok_tput_2nd

--- ovllm/llm.py
+++ ovllm/llm.py
@@ -208,7 +208,7 @@ class OVLLM(ABC):
         overhead_latency = gen_latency - token_total
         
         tok_tput_1st = input_batch_size * input_token_len / latency[0]
-        tok_tput_2nd = (input_batch_size * gen_sequence_length / sum(latency[1:])) if n_latency > 1 else 0
+        tok_tput_2nd = (input_batch_size * (gen_sequence_length - 1) / sum(latency[1:])) if n_latency > 1 else 0
 
         print(f"{self.title_tag}  [{input_batch_size}x{beam_size}, {input_token_len:4}+{gen_sequence_length}]  {gen_latency*1e3:.1f}ms = [{latency[0]*1e3:.1f}ms  {tok_tput_1st:.1f}tok/s] + [{latency[1]*1e3:.1f}ms + ({average_token_latency*1e3:.1f}ms x {n_latency-2})  {tok_tput_2nd:.1f}tok/s] + [{overhead_latency * 1e3:.1f}ms]")
 
